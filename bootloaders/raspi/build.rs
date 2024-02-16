265935160699c508dd4b7d1cad515245ca3769a1
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2024-02-16 17:33:53 -0600

    Get RPI version through rust features

--- bootloaders/raspi/build.rs
+++ bootloaders/raspi/build.rs
@@ -1,17 +1,4 @@
-use std::env;
-
 fn main() {
-   // Inform the linker of which version of the raspberry pi we are building for
-   match env::var("RPI").map(|x| {x.parse::<u8>().unwrap()}).unwrap_or(4) {
-      3 => { 
-         println!("cargo:rustc-link-arg=--defsym=__RPI_VER=3");
-      }
-      4 => { 
-         println!("cargo:rustc-link-arg=--defsym=__RPI_VER=4");
-      }
-      _ => panic!("Invalid RPI envvar"),
-   }
-
    // Pass in our custom linker script
    println!("cargo:rustc-link-arg=-Tbootloaders/raspi/linker.ld");
 }
--- bootloaders/raspi/src/main.rs
+++ bootloaders/raspi/src/main.rs
@@ -1,13 +1,19 @@
 #![no_std]
 #![no_main]
 
-use core::{arch::global_asm, ffi::c_void, panic::PanicInfo};
+use core::{arch::{asm, global_asm}, ffi::c_void, panic::PanicInfo};
 use kernel::kmain;
+
+#[cfg(feature = "raspi3")]
+static RASPI_VERSION: u8 = 3;
+#[cfg(feature = "raspi4")]
+static RASPI_VERSION: u8 = 4;
 
 global_asm!(include_str!("main.S"));
 
 #[no_mangle]
-pub extern "C" fn bootloader_main(dtb_ptr: *const c_void, rpi_version: u8) -> ! {  
+pub extern "C" fn bootloader_main(dtb_ptr: *const c_void) -> ! {  
+    unsafe { asm!("mov x21, {rpi}", rpi = in(reg) RASPI_VERSION as u64); }
     kmain()
 }
 
