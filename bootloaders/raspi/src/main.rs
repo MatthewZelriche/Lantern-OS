9e57b1f8d5b7af1b8f9bd51aa03989b1c86a9509
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2024-02-16 14:26:30 -0600

    Pass args to bootloader main

--- bootloaders/raspi/src/main.rs
+++ bootloaders/raspi/src/main.rs
@@ -1,13 +1,13 @@
 #![no_std]
 #![no_main]
 
-use core::{arch::global_asm, panic::PanicInfo};
+use core::{arch::global_asm, ffi::c_void, panic::PanicInfo};
 use kernel::kmain;
 
 global_asm!(include_str!("main.S"));
 
 #[no_mangle]
-pub extern "C" fn bootloader_main() -> ! {
+pub extern "C" fn bootloader_main(dtb_ptr: *const c_void, rpi_version: u8) -> ! {  
     kmain()
 }
 
