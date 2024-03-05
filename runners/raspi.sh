1a95c53b91a0aed6fc577a6de6de792b21562d62
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2024-03-04 20:30:55 -0600

    Fix building kernel binary

--- runners/raspi.sh
+++ runners/raspi.sh
@@ -4,9 +4,9 @@ mkdir -p out/
 rust-objcopy $1 -O binary out/kernel8.img
 
 if [ $2 = "raspi3" ] ; then
-   qemu-system-aarch64 -serial stdio -M raspi3b -kernel out/kernel8.img -dtb vendor/bcm2710-rpi-3-b.dtb
+   qemu-system-aarch64 -serial stdio -M raspi3b -kernel out/kernel8.img -dtb bootloaders/raspi/vendor/bcm2710-rpi-3-b.dtb
 elif [ $2 = "raspi4" ] ; then
-   ${QEMU_PATH}qemu-system-aarch64 -serial stdio -M raspi4b -kernel out/kernel8.img -dtb vendor/bcm2711-rpi-4-b.dtb
+   ${QEMU_PATH}qemu-system-aarch64 -serial stdio -M raspi4b -kernel out/kernel8.img -dtb bootloaders/raspi/vendor/bcm2711-rpi-4-b.dtb
 else
    echo "Invalid raspi board version specified"
 fi
